f677015fe46b6c61e02a9bca80947b1a676376f4
Mike Dame <damemi@gmail.com>
2022-04-22 14:30:21 -0400

    Chapter 4

---
+++ chapter_04/controllers/nginxoperator_controller.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	operatorv1alpha1 "github.com/sample/nginx-operator/api/v1alpha1"
+	"github.com/sample/nginx-operator/assets"
+)
+
+// NginxOperatorReconciler reconciles a NginxOperator object
+type NginxOperatorReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=operator.example.com,resources=nginxoperators,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=operator.example.com,resources=nginxoperators/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=operator.example.com,resources=nginxoperators/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the NginxOperator object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+func (r *NginxOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	operatorCR := &operatorv1alpha1.NginxOperator{}
+	err := r.Get(ctx, req.NamespacedName, operatorCR)
+	if err != nil && errors.IsNotFound(err) {
+		logger.Info("Operator resource object not found.")
+		return ctrl.Result{}, nil
+	} else if err != nil {
+		logger.Error(err, "Error getting operator resource object")
+		return ctrl.Result{}, err
+	}
+
+	deployment := &appsv1.Deployment{}
+	create := false
+	err = r.Get(ctx, req.NamespacedName, deployment)
+	if err != nil && errors.IsNotFound(err) {
+		create = true
+		deployment = assets.GetDeploymentFromFile("assets/nginx_deployment.yaml")
+	} else if err != nil {
+		logger.Error(err, "Error getting existing Nginx deployment.")
+		return ctrl.Result{}, err
+	}
+
+	deployment.Namespace = req.Namespace
+	deployment.Name = req.Name
+	if operatorCR.Spec.Replicas != nil {
+		deployment.Spec.Replicas = operatorCR.Spec.Replicas
+	}
+	if operatorCR.Spec.Port != nil {
+		deployment.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort = *operatorCR.Spec.Port
+	}
+	ctrl.SetControllerReference(operatorCR, deployment, r.Scheme)
+
+	if create {
+		err = r.Create(ctx, deployment)
+	} else {
+		err = r.Update(ctx, deployment)
+	}
+	return ctrl.Result{}, err
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *NginxOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&operatorv1alpha1.NginxOperator{}).
+		Owns(&appsv1.Deployment{}).
+		Complete(r)
+}
